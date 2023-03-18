0a6cd234e2c58aa6b05ab3dbe3425af9d5375f88
Italo Silva <italo@maleldil.com>
2023-03-18 19:18:33 +0000

    chatgpt: Add cost estimation script

---
+++ chatgpt/estimate_cost.py
@@ -0,0 +1,38 @@
+import argparse
+import json
+from pathlib import Path
+
+import tiktoken
+
+# $0.002 / 1K tokens
+PRICE_PER_TOKEN = 0.000002
+DEFAULT_PROMPT = "What are the causes, effects and relations in the following text?"
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=Path, default="../data/raw")
+    parser.add_argument("--prompt", type=str, default=DEFAULT_PROMPT)
+    args = parser.parse_args()
+
+    splits = ["train", "dev", "test"]
+    data: list[dict[str, str]] = []
+    for split in splits:
+        split_data = json.loads(
+            (args.input / f"event_dataset_{split}.json").read_text()
+        )
+        data.extend(split_data)
+
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+    data_tokens = sum(len(encoding.encode(d["info"])) for d in data)
+    prompt_tokens = len(encoding.encode(args.prompt)) * len(data)
+
+    total_tokens = data_tokens + prompt_tokens
+    cost = total_tokens * PRICE_PER_TOKEN
+
+    print(f"Estimated cost: ${cost:.2f} for {total_tokens} tokens")
+
+
+if __name__ == "__main__":
+    main()
