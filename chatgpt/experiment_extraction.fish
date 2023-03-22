9f9bb0094be208ac70642b84dc49c7ffa9d66f82
Italo Silva <italo@maleldil.com>
2023-03-22 18:37:41 +0000

    chatgpt(extraction): Improve argparser and add timestamp to exp script

--- chatgpt/experiment_extraction.fish
+++ chatgpt/experiment_extraction.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-argparse 'e/env=' -- $argv
+argparse 'env=' -- $argv
 or return
 
 if set -q _flag_env
@@ -9,17 +9,18 @@ else
     set environment dev
 end
 
+set timestamp (date -u +%Y-%m-%dT%H.%M.%SZ)
+
 set num_prompts 2
 set prompts (seq 0 (math $num_prompts - 1))
-
 set modes tags lines
 
-if [ $environment = dev ]
-    set input_file "extraction_dev_2.json"
-    set examples_file "extraction_examples_3.json"
-else
+if [ $environment = exp ]
     set input_file "extraction_dev_full.json"
     set examples_file "extraction_examples.json"
+else
+    set input_file "extraction_dev_2.json"
+    set examples_file "extraction_examples_3.json"
 end
 
 mkdir -p output
@@ -27,8 +28,9 @@ for prompt in $prompts
     for mode in $modes
         set input ./data/$mode/$input_file
         set examples ./data/$mode/$examples_file
-        set output ./output/{$mode}_{$environment}_prompt{$prompt}_output.json
-        set metrics ./output/{$mode}_{$environment}_prompt{$prompt}_metrics.json
+
+        set output ./output/{$mode}_{$environment}_prompt{$prompt}_output.{$timestamp}.json
+        set metrics ./output/{$mode}_{$environment}_prompt{$prompt}_metrics.{$timestamp}.json
 
         echo $prompt
         echo "input $input"
