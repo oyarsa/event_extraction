0a2c826bb455fbcb1e47f323c833802cfe3f2320
Italo Silva <italo@maleldil.com>
2023-05-27 00:02:01 +0100

    self_critique: Add scripts to generate confusion matrix
    Also per-class accuracy. Done in both Python and Clojure because
    I can.

---
+++ self_critique/scripts/confusion_matrix.py
@@ -0,0 +1,69 @@
+import json
+from collections import Counter
+from pathlib import Path
+
+import numpy as np
+import typer
+from sklearn.metrics import confusion_matrix
+
+
+def class_acc(y_true: list[str], y_pred: list[str]) -> dict[str, int]:
+    true_counts = Counter(y_true)
+    class_accuracies: dict[str, int] = {}
+
+    for label in true_counts:
+        true_count = true_counts[label]
+        correct_count = sum(
+            1
+            for true_label, pred_label in zip(y_true, y_pred)
+            if true_label == label and pred_label == label
+        )
+        class_accuracies[label] = correct_count / true_count
+
+    return class_accuracies
+
+
+def print_confusion(cm: np.ndarray, labels: list[str]) -> None:
+    label_width = max(len(label) for label in labels)
+
+    # Print header row with class labels
+    header = "  ".join(label.ljust(label_width) for label in labels)
+    top_left_corner = "T\\P".center(label_width)
+    print(f"{top_left_corner}  {header}")
+
+    # Print each row of the confusion matrix
+    for i, row in enumerate(cm):
+        row_str = "  ".join(str(val).ljust(label_width) for val in row)
+        print(f"{labels[i].ljust(label_width)}  {row_str}")
+
+
+def print_acc(accuracies: dict[str, int]) -> None:
+    print("\nCLASS ACCURACY")
+    label_width = max(len(label) for label in accuracies) + 1
+    for label, rate in accuracies.items():
+        print(f"{label.ljust(label_width)}: {rate:.2%}")
+
+
+def main(file: Path) -> None:
+    """Print confusion matrix and class accuracies from a JSON file.
+
+    Usage: python scripts/confusion_matrix.py FILE
+
+    FILE should be a JSON file containing a list of objects with "gold" and "prediction"
+    keys. The values should be strings representing class labels.
+    """
+    data = json.loads(file.read_text())
+
+    y_true = [d["gold"] for d in data]
+    y_pred = [d["prediction"] for d in data]
+    labels = list(set(y_true + y_pred))
+
+    confusion = confusion_matrix(y_true, y_pred, labels=labels)
+    print_confusion(confusion, labels)
+
+    acc = class_acc(y_true, y_pred)
+    print_acc(acc)
+
+
+if __name__ == "__main__":
+    typer.run(main)
