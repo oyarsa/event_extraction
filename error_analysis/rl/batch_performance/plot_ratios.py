bd1e9efe07ed4db698ba61ead6db217e1e6e9664
Italo Silva <italo@maleldil.com>
2023-06-06 15:55:40 +0100

    self_critique(scripts): Add scripts to visualise RL performance over time

---
+++ error_analysis/rl/batch_performance/plot_ratios.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pixcat
+import typer
+
+
+def main(data_path: Path, n: int) -> None:
+    data = pd.read_csv(data_path, header=None, names=["batch", "ratio"])
+    plt.xlabel("batch")
+    plt.ylabel("entailment ratio")
+    plt.xticks(
+        np.arange(min(data.batch), max(data.batch) + 1, 10), fontsize=8, rotation=30
+    )
+    plt.grid()
+
+    groups = list(data.groupby("batch"))
+    chunks = [
+        pd.concat([group for _, group in groups[i : i + n]])
+        for i in range(0, len(groups), n)
+    ]
+
+    colormap = plt.colormaps.get_cmap("viridis")
+    for i, chunk in enumerate(chunks):
+        color = colormap(i / len(chunks))
+        plt.plot(chunk.batch, chunk.ratio, color=color)
+        plt.plot([], [], color=color, label=f"Epoch {i+1}")
+
+    plt.legend(loc="upper right", title="Legend")
+
+    plt.savefig("output.png")
+    pixcat.Image("output.png").fit_screen(enlarge=True).show()
+
+
+if __name__ == "__main__":
+    typer.run(main)
