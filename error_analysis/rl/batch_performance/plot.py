4a5a8ae7bec8eb035bafe0b7cd238803b9454ce1
Italo Silva <italo@maleldil.com>
2023-08-22 08:01:50 +0100

    erorr_analysis(rl): Fix plot.py to work with less granular input
    It wasn't colouring the epochs correctly because it was assuming that there
    was always a row for each epoch, which is not necessarily true.

--- error_analysis/rl/batch_performance/plot.py
+++ error_analysis/rl/batch_performance/plot.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import cast
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,26 +8,25 @@ import pixcat
 import typer
 
 
-def main(data_path: Path, n: int, y_label: str) -> None:
+def main(data_path: Path, batches_per_epoch: int, y_label: str) -> None:
     data = pd.read_csv(data_path, header=None, names=["batch", y_label])
     plt.xlabel("batch")
     plt.ylabel(y_label)
     plt.xticks(
-        np.arange(min(data.batch), max(data.batch) + 1, 20), fontsize=8, rotation=30
+        np.arange(min(data.batch), max(data.batch) + 1, 50), fontsize=8, rotation=30
     )
     plt.grid()
 
-    groups = list(data.groupby("batch"))
-    chunks = [
-        pd.concat([group for _, group in groups[i : i + n]])
-        for i in range(0, len(groups), n)
-    ]
+    bins = range(0, max(data.batch) + batches_per_epoch, batches_per_epoch)
+    data["epoch"] = pd.cut(data["batch"], bins=bins, labels=False, include_lowest=True)
 
     colormap = plt.colormaps.get_cmap("viridis")
-    for i, chunk in enumerate(chunks):
-        color = colormap(i / len(chunks))
-        plt.plot(chunk["batch"], chunk[y_label], color=color)
+    for epoch_, group in data.groupby("epoch"):
+        epoch = cast(int, epoch_)
+        color = colormap(epoch / (len(bins) - 1))
+        plt.plot(group["batch"], group[y_label], color=color, label=f"Epoch {epoch}")
 
+    plt.legend(loc="best")
     plt.savefig("output.png", dpi=600)
     pixcat.Image("output.png").fit_screen(enlarge=True).show()
 
