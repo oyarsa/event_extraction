204695e12423cdda4acf5be9433af67ec8b87aa1
Italo Silva <italo@maleldil.com>
2023-09-13 18:22:02 +0100

    self_critique(scripts): Fix label_dist.py to use first argument as label

--- self_critique/scripts/label_dist.py
+++ self_critique/scripts/label_dist.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 # pyright: basic
+import argparse
 import json
 import sys
 from collections import Counter
 
-label = sys.argv[1] if len(sys.argv) > 1 else "label"
+parser = argparse.ArgumentParser()
+parser.add_argument("label", type=str)
+
+args = parser.parse_args()
 data = json.load(sys.stdin)
 
-preds = [d["label"] for d in data]
+preds = [d[args.label] for d in data]
 freqs = Counter(preds)
+
+print(f"Total: {len(preds)}\n")
 
 for k, v in freqs.items():
     print(f"{k}: {v} ({v/len(preds):.2%})")
