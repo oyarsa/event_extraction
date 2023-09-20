6e79256f17c924439c9d5f4670dbc78f48da196e
Italo Silva <italo@maleldil.com>
2023-09-20 14:40:51 +0100

    self_critique(scripts): Clean-up some scripts
    - label_dist.fish wasn't necessary given label_dist.py, which has the same
    features and is easier to expand.
    - Changed all_label_dist.fish to use label_dist.py and print the filename since
      the Python version doesn't do that

--- self_critique/scripts/all_label_dist.fish
+++ self_critique/scripts/all_label_dist.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 
-set label_dist (dirname (status --current-filename))/label_dist.fish
+set label_dist (dirname (status --current-filename))/label_dist.py
 for f in $argv
+    echo $f
     $label_dist $f
     echo
 end
--- self_critique/scripts/label_dist.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-set file $argv[1]
-echo $file
-jq 'map(.entailment_label) | sort | .[]' $file | uniq -c
