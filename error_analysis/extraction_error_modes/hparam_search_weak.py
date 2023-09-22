589e5196d7e701f2b6c0dbc44abe59a4bd043631
Italo Silva <italo@maleldil.com>
2023-09-22 15:18:07 +0100

    error_analysis(extraction): Change setup for hparam for weak

--- error_analysis/extraction_error_modes/hparam_search_weak.py
+++ error_analysis/extraction_error_modes/hparam_search_weak.py
@@ -10,59 +10,76 @@ from tabulate import tabulate
 from tqdm import tqdm
 
 # Hyperparameters to search over
-# model_names = ["microsoft/deberta-base"]
-# learning_rates = [1e-5]
-# max_seq_lengths = [256, 512]
-model_names = ["microsoft/deberta-base", "microsoft/deberta-v3-base"]
-learning_rates = [1e-5, 5e-5, 1e-4]
-max_seq_lengths = [256, 512]
+model_names = ["t5-base", "google/flan-t5-base"]
+learning_rates = [1e-5, 5e-5, 1e-4, 1e-3]
+max_seq_lengths = [256]
+train_data_paths = [
+    "./training/weakly.json",
+    "./training/resampled.json",
+    "./training/transformed/weakly.json",
+]
 
 # Other (fixed) hyperparameters
-train_data_path = "./training/weakly.json"
+# train_data_path = "./training/weakly.json"
 eval_data_path = "./evaluation/all_labelled.json"
 test_data_path = "./prediction/all_labelled.json"
 infer_data_path = "./prediction/all_labelled.json"
-# num_epochs = 100
-num_epochs = 2
+# num_epochs = 2
+num_epochs = 100
 early_stopping_patience = 10
 # max_samples = 100
 max_samples = None
-output_path = "output/hparam_weak"
+output_path = "output/hparam_weak_t5"
 do_train = True
 do_evaluation = True
 do_test = False
 do_inferece = False
 
 print(">>> Fixed hyperparameters")
-print(f"num_epochs: {num_epochs}")
-print(f"early_stopping_patience: {early_stopping_patience}")
-print(f"max_samples: {max_samples}")
-print(f"output_path: {output_path}")
-print(f"train_data_path: {train_data_path}")
-print(f"eval_data_path: {eval_data_path}")
-print(f"test_data_path: {test_data_path}")
-print(f"infer_data_path: {infer_data_path}")
+print(f"{num_epochs=}")
+print(f"{early_stopping_patience=}")
+print(f"{max_samples=}")
+print(f"{output_path=}")
+# print(f"{train_data_path=}")
+print(f"{eval_data_path=}")
+print(f"{test_data_path=}")
+print(f"{infer_data_path=}")
 print()
 
 all_metrics: list[dict[str, Any]] = []
-total_combinations = len(model_names) * len(learning_rates) * len(max_seq_lengths)
+total_combinations = (
+    len(model_names)
+    * len(learning_rates)
+    * len(max_seq_lengths)
+    * len(train_data_paths)
+)
 
-for i, (model_name, learning_rate, max_seq_length) in enumerate(
-    tqdm(product(model_names, learning_rates, max_seq_lengths))
+for i, (model_name, learning_rate, max_seq_length, train_data_path) in enumerate(
+    tqdm(product(model_names, learning_rates, max_seq_lengths, train_data_paths))
 ):
-    batch_size = 16 if max_seq_length == 512 else 32
+    if max_seq_length == 512:
+        batch_size = 16
+    elif "small" in model_name:
+        batch_size = 64
+    else:
+        batch_size = 32
+    train_name = {
+        "./training/weakly.json": "standard",
+        "./training/resampled.json": "resampled",
+        "./training/transformed/weakly.json": "transformed",
+    }[train_data_path]
+
     print(f"\n\n>>>> {i+1} / {total_combinations} <<<<")
     print(
-        f"Model: {model_name}, learning rate: {learning_rate}, max_seq_length:"
-        f" {max_seq_length}, batch_size: {batch_size}\n"
+        f"{model_name=} {learning_rate=} {max_seq_length=} {batch_size=} {train_name=}"
     )
 
     model_name_for_path = model_name.replace("/", ".")
-    output_name = f"{model_name_for_path}_{learning_rate}_{max_seq_length}"
+    output_name = f"{model_name_for_path}_{learning_rate}_{max_seq_length}_{train_name}"
 
     args = [
         sys.executable,
-        "classifier.py",
+        "classifier_t5.py",
         "--model_name",
         model_name,
         "--learning_rate",
