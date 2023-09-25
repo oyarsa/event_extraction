3420828f67c1672c14a6896c5107814184e422ee
Italo Silva <italo@maleldil.com>
2023-09-25 18:13:50 +0100

    error_analysis(extraction): Refactor show_metrics.fish a little bit
    Run jq only once to get all metrics

--- error_analysis/extraction_error_modes/scripts/show_metrics.fish
+++ error_analysis/extraction_error_modes/scripts/show_metrics.fish
@@ -4,8 +4,5 @@ for f in (fd training_metrics.jsonl $argv[1])
     printf "%-45s %-8d %-7.5f %-10.5f %-10.5f %-10.5f\n" \
         (basename (dirname $f)) \
         (wc -l < $f | string trim) \
-        (jq -s 'max_by(.accuracy).accuracy' $f) \
-        (jq -s 'max_by(.accuracy).f1' $f) \
-        (jq -s 'max_by(.accuracy).precision' $f) \
-        (jq -s 'max_by(.accuracy).recall' $f)
+        (jq -s 'max_by(.accuracy) | .accuracy, .f1, .precision, .recall' $f)
 end | sort -k2
