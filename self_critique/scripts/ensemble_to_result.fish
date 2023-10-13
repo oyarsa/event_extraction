55c65f900ee2c37bdc87b57d8481c1309d93abf3
Italo Silva <italo@maleldil.com>
2023-10-13 19:27:25 +0100

    self_critique(scripts): Refactor ensemble_to_result.fish
    Now it uses stdin if no parameter is provided, or uses the parameter
    as the file name if it is.
    
    ChatGPT tought me this trick of using 'cat -' to read stdin, so I can
    just define the input variable instead of doing tricks like reading (cat)
    to a string.
    
    I'm probably to take this with me moving forward because I think this
    combination is good. I don't have to create a temp pipe with psub like
    when the scripts expects a file name, and I can't forget to <file when
    the scripts only reads from stdin.
    
    Usability win, yay.

--- self_critique/scripts/ensemble_to_result.fish
+++ self_critique/scripts/ensemble_to_result.fish
@@ -1,2 +1,4 @@
 #!/usr/bin/env fish
-jq 'map({input: .context, gold: .answers, output: .text_chosen})'
+
+set -q argv[1]; and set input $argv[1]; or set input -
+cat $input | jq 'map({input: .context, gold: .answers, output: .text_chosen})'
