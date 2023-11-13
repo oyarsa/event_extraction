77eb729f5d6f8634b87f786325c81a0b0e8f8695
Italo Silva <italo@maleldil.com>
2023-11-13 11:13:24 +0000

    chatgpt(extraction): Add support to GPT-4 and its prompts

--- chatgpt/extraction.py
+++ chatgpt/extraction.py
@@ -23,8 +23,42 @@ from metrics import (
     calculate_metrics,
 )
 
-EXTRACTION_PROMPTS = [
-    "What are the causes, effects and relations in the following text?",  # 0
+USER_PROMPTS = [
+    # 0
+    "What are the causes, effects and relations in the following text?",
+    # 1
+    """\
+What are the causes, effects and relation in the following text?
+The relation must be one of "cause", "enable", or "prevent".
+The causes and effects must be spans of the text. There is only one relation.
+
+The response should be formatted as this:
+Cause: <text>
+Effect: <text>
+Relation: <text>
+
+When there are multiple causes or effects, separate them by " | ". Don't add quotes around the extractions.
+""",
+    # 2
+    """\
+What are the causes, effects and relation in the following text?
+The relation must be one of "cause", "enable", or "prevent".
+The causes and effects must be spans of the text. There is only one relation.
+
+The response should be formatted as this:
+`[Cause] <cause text> [Relation] <relation> [Effect] <effect text>`
+
+When there are multiple causes or effects, separate them by " | ". Don't add quotes around the extractions.
+""",
+]
+SYSTEM_PROMPTS = [
+    # 0
+    "You are a helpful assistant that extract causes, effects, and"
+    " relations from text. The format is `[Cause] <cause text> [Relation]"
+    " <relation> [Effect] <effect text>`.",
+    # 1
+    "You are a helpful assistant that extract causes, effects, and a relation from"
+    " text.",
 ]
 
 
@@ -32,11 +66,14 @@ def make_extraction_request(
     model: str,
     text: str,
     examples: list[dict[str, str]],
-    prompt: str,
+    user_prompt: str,
+    system_prompt: str,
 ) -> dict[str, Any]:
     return make_chat_request(
         model=model,
-        messages=generate_extraction_messages(text, examples, prompt),
+        messages=generate_extraction_messages(
+            text, examples, user_prompt, system_prompt
+        ),
     )
 
 
@@ -51,17 +88,18 @@ def gen_extraction_example_exchange(
 
 
 def generate_extraction_messages(
-    text: str, examples: list[dict[str, str]], prompt: str
+    text: str,
+    examples: list[dict[str, str]],
+    user_prompt: str,
+    system_prompt: str,
 ) -> list[dict[str, str]]:
     return [
         make_msg(
             "system",
-            "You are a helpful assistant that extract causes, effects, and"
-            " relations from text. The format is `[Cause] <cause text> [Relation]"
-            " <relation> [Effect] <effect text>`.",
+            system_prompt,
         ),
-        *gen_extraction_example_exchange(examples, prompt),
-        make_msg("user", prompt),
+        *gen_extraction_example_exchange(examples, user_prompt),
+        make_msg("user", user_prompt),
         make_msg("user", text),
     ]
 
@@ -77,14 +115,19 @@ def extract_clauses(
     demonstration_examples: list[dict[str, str]],
     inputs: list[MetricReference],
     extraction_mode: StructureFormat,
-    prompt: str,
+    user_prompt: str,
+    system_prompt: str,
 ) -> ExtractionResult:
     responses: list[dict[str, Any]] = []
     predictions: list[MetricPrediction] = []
 
     for example in tqdm(inputs):
         response = make_extraction_request(
-            model, example["context"], demonstration_examples, prompt
+            model,
+            example["context"],
+            demonstration_examples,
+            user_prompt,
+            system_prompt,
         )
         pred: MetricPrediction = {
             "id": example["id"],
@@ -100,14 +143,19 @@ def extract_clauses(
 
 def run_extraction(
     model: str,
-    demonstration_examples_path: Path,
+    demonstration_examples_path: Path | None,
     input_path: Path,
     output_path: Path | None,
     metric_path: Path | None,
     extraction_mode: StructureFormat,
-    prompt: str,
+    user_prompt: str,
+    system_prompt: str,
 ) -> None:
-    demonstration_examples = json.loads(demonstration_examples_path.read_text())
+    if demonstration_examples_path is not None:
+        demonstration_examples = json.loads(demonstration_examples_path.read_text())
+    else:
+        demonstration_examples = []
+
     examples: list[MetricReference] = json.loads(input_path.read_text())["data"]
 
     responses, predictions, metrics = extract_clauses(
@@ -115,7 +163,8 @@ def run_extraction(
         demonstration_examples=demonstration_examples,
         inputs=examples,
         extraction_mode=extraction_mode,
-        prompt=prompt,
+        user_prompt=user_prompt,
+        system_prompt=system_prompt,
     )
 
     output = [
@@ -133,10 +182,9 @@ def run_extraction(
         print(json.dumps(output, indent=2))
 
     metrics["cost"] = sum(calculate_cost(model, response) for response in responses)
-    if metric_path is None:
-        print("\nMetrics:")
-        print(json.dumps(metrics, indent=2))
-    else:
+    print("\nMetrics:")
+    print(json.dumps(metrics, indent=2))
+    if metric_path is not None:
         metric_path.write_text(json.dumps(metrics, indent=2))
 
 
@@ -145,7 +193,6 @@ def main() -> None:
     parser.add_argument(
         "--examples",
         type=Path,
-        default="data/tags/extraction_examples_3.json",
         help="The path to file with the demonstration examples.",
     )
     parser.add_argument(
@@ -161,8 +208,11 @@ def main() -> None:
     logger.config(args.log_file, args.print_logs)
     openai.api_key = get_key(args.key_file, args.key_name)
 
-    if args.prompt < 0 or args.prompt >= len(EXTRACTION_PROMPTS):
-        raise IndexError(f"Invalid prompt index: {args.prompt}")
+    if args.prompt < 0 or args.prompt >= len(USER_PROMPTS):
+        raise IndexError(f"Invalid user prompt index: {args.prompt}")
+
+    if args.sys_prompt < 0 or args.sys_prompt >= len(USER_PROMPTS):
+        raise IndexError(f"Invalid system prompt index: {args.prompt}")
     run_extraction(
         model=args.model,
         demonstration_examples_path=args.examples,
@@ -170,7 +220,8 @@ def main() -> None:
         output_path=args.output,
         metric_path=args.metrics_path,
         extraction_mode=args.mode,
-        prompt=EXTRACTION_PROMPTS[args.prompt],
+        user_prompt=USER_PROMPTS[args.prompt],
+        system_prompt=SYSTEM_PROMPTS[args.sys_prompt],
     )
 
 
