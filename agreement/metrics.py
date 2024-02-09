5f4e30696965fa3c45368d69fe73398151c072ad
Italo Silva <italo@maleldil.com>
2024-02-09 14:50:24 +0000

    agreement: Improve types in calculate_metric

--- agreement/metrics.py
+++ agreement/metrics.py
@@ -1,23 +1,25 @@
-from typing import Any
+from typing import SupportsInt
 
 import krippendorff
 from scipy.stats import spearmanr
 from sklearn.metrics import cohen_kappa_score
 
-AVAILABLE_METRICS = ["agreement", "krippendorff", "kdf", "spearman", "cohen"]
+AVAILABLE_METRICS = ["agreement", "krippendorff", "spearman", "cohen"]
 
 
-def calculate_metric(metric: str, x: list[Any], y: list[Any]) -> float:
+def calculate_metric(
+    metric: str, xs: list[SupportsInt], ys: list[SupportsInt]
+) -> float:
     if metric not in AVAILABLE_METRICS:
         raise ValueError(f"Unknown metric: {metric}")
 
-    x = [int(value) for value in x]
-    y = [int(value) for value in y]
+    x = [int(value) for value in xs]
+    y = [int(value) for value in ys]
 
     match metric:
         case "agreement":
             return sum(a == b for a, b in zip(x, y)) / len(x)
-        case "krippendorff" | "kdf":
+        case "krippendorff":
             return krippendorff.alpha([x, y], level_of_measurement="nominal")
         case "spearman":
             return spearmanr(x, y)[0]
