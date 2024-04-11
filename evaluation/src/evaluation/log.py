e995a3c6c3e9fb23c95a48241d5175d1eb3db82f
Italo Silva <italo@maleldil.com>
2024-04-11 14:21:54 +0100

    evaluation: Add more options to `setup_logger`

--- evaluation/src/evaluation/log.py
+++ evaluation/src/evaluation/log.py
@@ -3,14 +3,19 @@ import sys
 from pathlib import Path
 
 
-def setup_logger(logger: logging.Logger, output_dir: Path) -> None:
+def setup_logger(
+    logger: logging.Logger,
+    output_dir: Path,
+    file_name: str = "train.log",
+    mode: str = "a",
+) -> None:
     logger.setLevel(logging.INFO)
 
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)s | %(name)s | %(message)s", "%Y-%m-%d %H:%M:%S"
     )
 
-    file_handler = logging.FileHandler(output_dir / "train.log")
+    file_handler = logging.FileHandler(output_dir / file_name, mode=mode)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
