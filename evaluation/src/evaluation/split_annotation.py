0429b04e957e92f108e9115188a3def8d3356b5e
Italo Silva <italo@maleldil.com>
2024-05-07 15:38:17 +0100

    evaluation(ann): Add script to split dataset for annotation
    This script splits a dataset into multiple subsets for annotation,
    ensuring
    that a common subset is shared between them. This allows us to calculate
    the inter-annotator agreement while still having different subsets for
    each annotator.

---
+++ evaluation/src/evaluation/split_annotation.py
@@ -0,0 +1,81 @@
+"""Split dataset into subsets for annotation, including a common subset between them.
+
+Ensures that the resulting datasets have a subset of data that is the same between them
+so we can calculate the inter-annotator agreement.
+"""
+import argparse
+import json
+import random
+from pathlib import Path
+from typing import Any
+
+
+def split_data(
+    data: list[dict[str, Any]], num_subsets: int, common_pct: float
+) -> list[list[dict[str, Any]]]:
+    data_ = data.copy()
+    random.shuffle(data_)
+
+    common_size = int(len(data_) * common_pct)
+    common, remaining = data_[:common_size], data_[common_size:]
+    size = len(remaining) // num_subsets
+
+    return [common + remaining[i * size : (i + 1) * size] for i in range(num_subsets)]
+
+
+def main(
+    input: Path, output_dir: Path, num_subsets: int, common_pct: float, seed: int
+) -> None:
+    random.seed(seed)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    data = [
+        {
+            "text": item["input"],
+            "model": item["output"],
+            "annotation": item["gold"],
+        }
+        for item in json.loads(input.read_text())
+    ]
+    splits = split_data(data, num_subsets, common_pct)
+    print(f"Split length: {len(splits[0])} x {num_subsets} ({common_pct:.0%} common)")
+
+    for i, subset in enumerate(splits):
+        path = output_dir / f"{input.name}_subset_{i}.json"
+        path.write_text(json.dumps(subset, indent=2))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__.splitlines()[0],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("input", type=Path, help="Input JSON file")
+    parser.add_argument(
+        "output_dir", type=Path, help="Output directory to save the split datasets"
+    )
+    parser.add_argument(
+        "--num-subsets",
+        "-n",
+        type=int,
+        default=2,
+        metavar="N",
+        help="Number of subsets to create",
+    )
+    parser.add_argument(
+        "--common-pct",
+        "-p",
+        type=float,
+        default=0.2,
+        metavar="PCT",
+        help="Percentage of data to share between subsets",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=0,
+        help="Random seed for reproducibility",
+    )
+    args = parser.parse_args()
+
+    main(args.input, args.output_dir, args.num_subsets, args.common_pct, args.seed)
