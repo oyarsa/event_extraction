b2edb365edeed15f5414948147e0302d08e82b87
Italo Silva <italo@maleldil.com>
2024-05-12 01:59:50 +0100

    annotation: Remove cookies because all libs are broken
    So I'm back to current page only. I'll try the query param thing once
    again.

--- annotation/src/annotation/Start_Page.py
+++ annotation/src/annotation/Start_Page.py
@@ -1,4 +1,3 @@
-import extra_streamlit_components as stx
 import streamlit as st
 
 from annotation.common import (
@@ -9,25 +8,10 @@ from annotation.common import (
 )
 
 
-# I'm not sure why the experimental flag is needed since it's not in the example.
-# It _looks like_ it works.
-@st.cache_resource(experimental_allow_widgets=True)
-def get_manager() -> stx.CookieManager:
-    return stx.CookieManager()
-
-
-_PROLIFIC_COOKIE_KEY = "prolific_id"
-
-
 def main() -> None:
     st.set_page_config(page_title="Event Extraction Annotation")
 
-    cookies = get_manager()
-
     st.title("Welcome")
-
-    if prolific_id := cookies.get(_PROLIFIC_COOKIE_KEY):
-        set_prolific_state_id(prolific_id)
 
     if get_prolific_id():
         st.write(
@@ -35,7 +19,6 @@ def main() -> None:
             " instructions."
         )
         if st.button("Log out", type="primary"):
-            cookies.delete(_PROLIFIC_COOKIE_KEY)
             reset_prolific_id()
             st.rerun()
 
@@ -48,7 +31,6 @@ def main() -> None:
         placeholder="Prolific ID",
     ):
         set_prolific_state_id(prolific_id)
-        cookies.set(_PROLIFIC_COOKIE_KEY, prolific_id)
         st.rerun()
 
 
