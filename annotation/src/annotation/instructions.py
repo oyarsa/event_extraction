1c9a39239134d03954bb27a444bfa3648781e2fe
Italo Silva <italo@maleldil.com>
2024-05-12 12:57:09 +0100

    annotation: Inline instructions
    The idea was to embed the instructions in multiple pages, but I don't
    think I want that anymore.

--- annotation/src/annotation/instructions.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-def render_instructions() -> None:
-    st.title("Instructions")
-    st.write("TODO")
--- annotation/src/annotation/pages/1_Instructions.py
+++ annotation/src/annotation/pages/1_Instructions.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 from annotation.common import colour, get_prolific_id
-from annotation.instructions import render_instructions
 
 
 def main() -> None:
     get_prolific_id()
-    render_instructions()
+    st.title("Instructions")
+    st.write("TODO")
 
     st.page_link(
         "pages/2_Annotation.py", label=colour("Go to annotation page", bg="green")
