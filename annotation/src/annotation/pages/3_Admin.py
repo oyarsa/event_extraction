131861c0809692d201cd706e04ad3d9a29467190
Italo Silva <italo@maleldil.com>
2024-05-13 14:04:02 +0100

    annotation: Implement admin page to upload files
    I wanted a multi-upload thing, but that would require multiple
    confirmations if the files were to be ovewritten, and it didn't seem
    worth battling Stremalit for this. This is a little more work, but it
    should be fine.

--- annotation/src/annotation/pages/3_Admin.py
+++ annotation/src/annotation/pages/3_Admin.py
@@ -1,11 +1,29 @@
+import json
+
 import streamlit as st
-from evaluation.gpt_common import hashlib
 
-from annotation.common import get_prolific_id
+from annotation.common import get_config, get_prolific_id
 
 
-def hashkey(password: str) -> str:
-    return hashlib.sha256(password.encode("utf-8")).hexdigest()
+def validate_file(file_bytes: bytes, keys: list[str]) -> str | None:
+    """Check if the file is a valid JSON with the correct format.
+
+    Returns the error message if invalid, otherwise None.
+    """
+    try:
+        data = json.loads(file_bytes)
+    except json.JSONDecodeError:
+        return "Invalid JSON file"
+
+    if not isinstance(data, list):
+        return "JSON file should be a list"
+
+    valid = all(
+        isinstance(item, dict) and all(key in item for key in keys) for item in data
+    )
+    if not valid:
+        return f"Every item should be an object with keys {keys}"
+    return None
 
 
 def main() -> None:
@@ -14,11 +32,30 @@ def main() -> None:
         return
 
     if prolific_id != "admin":
-        st.error("You are not an admin")
+        st.error("You are not the admin.")
         return
 
     st.header("Admin panel")
-    st.write("TODO")
+
+    file = st.file_uploader("Choose a JSON data file")
+    if not file:
+        return
+
+    file_path = get_config().annotation_dir / file.name
+    if file_path.exists():
+        st.warning(
+            f"File {file_path} already exists. Confirm if you want to overwrite."
+        )
+        if not st.button("Overwrite", key=f"confirm_{file.name}"):
+            return
+
+    keys = ["text", "annotation", "model"]
+    if error := validate_file(file.getvalue(), keys):
+        st.error(error)
+        return
+
+    file_path.write_bytes(file.getvalue())
+    st.write(f"Uploaded {file_path}")
 
 
 if __name__ == "__main__":
