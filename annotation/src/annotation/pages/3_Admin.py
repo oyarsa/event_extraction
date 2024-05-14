bfb991cdda6fb24fc10777c73ee6e3ec2c509b6c
Italo Silva <italo@maleldil.com>
2024-05-14 16:36:27 +0100

    annotation: Fix upload's update of the split-to-user file
    It was saving the file _name_ instead of the _stem_. The map file
    doesn't contain the extension, so it was giving spurious errors.
    
    I'm not entirely sure how it was still loading the correct file even
    with that error. But it works.

--- annotation/src/annotation/pages/3_Admin.py
+++ annotation/src/annotation/pages/3_Admin.py
@@ -75,7 +75,7 @@ def main(annotation_dir: Path, split_to_user_file: Path) -> None:
 
     file_path.write_bytes(file.getvalue())
     split_to_user = json.loads(split_to_user_file.read_text())
-    split_to_user[file_path.name] = None
+    split_to_user[file_path.stem] = None
     backup_and_write(split_to_user_file, json.dumps(split_to_user, indent=2))
 
     logger.info("Uploaded %s and updated the source-to-user file.", file_path)
