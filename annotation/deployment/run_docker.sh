016323a0634380c7d2cf1f56f2c72a608d87f0a3
Italo Silva <italo@maleldil.com>
2024-05-20 15:30:25 +0100

    annotation(deploy): Add nginx conf and script to run docker

---
+++ annotation/deployment/run_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+# Replace with volume path
+files="/mnt/volume_ams3_01/files"
+
+# --user to set user so that I can modify/move/delete the docker-created files
+docker run -p 8501:8501 \
+	--restart unless-stopped \
+	--user 1000:1000 \
+	-v "$files/config":/app/config \
+	-v "$files/data":/app/data \
+	-v "$files/logs":/app/logs \
+	annotation
