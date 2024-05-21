4ed240b291d5b31920766831fce65d5b7db4f23d
Italo Silva <italo@maleldil.com>
2024-05-21 11:03:56 +0100

    chatgpt(scripts): Add better CLI handling for tags -> lines conversion script
    Includes optional relation for FinCausal.

--- chatgpt/scripts/convert_tags_to_lines.py
+++ chatgpt/scripts/convert_tags_to_lines.py
@@ -23,42 +23,71 @@ If the data is inside a key, use the key as the first argument:
 python scripts/convert_tags_to_lines.py data < input.json > output.json
 ```
 """
+
+import argparse
 import json
 import sys
 from pathlib import Path
+from typing import TextIO
 
 sys.path.append(str(Path(__file__).parents[1]))
 from metrics import parse_instance_tags
 
 
-def convert_answer(item: str) -> str:
+def convert_answer(item: str, add_relation: bool) -> str:
     entities, relation = parse_instance_tags(item)
     rows = [
         f'Cause: {"| ".join(entities["Cause"])}',
         f'Effect: {"| ".join(entities["Effect"])}',
-        f"Relation: {relation}",
     ]
+    if add_relation:
+        rows.append(f"Relation: {relation}")
     return "\n".join(rows)
 
 
-def convert_item(item: dict[str, str]) -> dict[str, str]:
-    return {**item, "answers": convert_answer(item["answers"])}
+def convert_item(item: dict[str, str], add_relation: bool) -> dict[str, str]:
+    return {**item, "answers": convert_answer(item["answers"], add_relation)}
 
 
-def main() -> None:
-    data = json.load(sys.stdin)
+def main(input_file: TextIO, key: str | None, add_relation: bool) -> None:
+    data = json.load(input_file)
     new_data: dict[str, list[dict[str, str]]] | list[dict[str, str]]
-    if len(sys.argv) >= 2:
-        key = sys.argv[1]
+
+    if key is not None:
         new_data = {
             **data,
-            "data": [convert_item(item) for item in data[key]],
+            "data": [convert_item(item, add_relation) for item in data[key]],
         }
     else:
-        new_data = [convert_item(item) for item in data]
+        new_data = [convert_item(item, add_relation) for item in data]
 
     print(json.dumps(new_data, indent=2))
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description=__doc__.splitlines()[0],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "input_file",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        nargs="?",
+        help="Input file",
+    )
+    parser.add_argument(
+        "--key",
+        type=str,
+        default=None,
+        help="Key to extract the data from, if the file isn't a list",
+    )
+    parser.add_argument(
+        "--add-relation",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Add the relation to the output. Should be True for FCR and False for"
+        " FinCausal",
+    )
+    args = parser.parse_args()
+    main(args.input_file, args.key, args.add_relation)
