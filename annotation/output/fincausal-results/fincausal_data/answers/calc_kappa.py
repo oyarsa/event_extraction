cac63b35678f3973386461a85042e16b2e356a62
Italo Silva <italo@maleldil.com>
2024-05-28 13:30:35 +0100

    annotation(output): calc_kappa now save the order of "most agreeable"
    This means calculating the average pairwise Kappa for each person in the
    cohort, and saving the ordered list (highest score first) to a file,
    where it will be used to break ties later.

--- annotation/output/fincausal-results/fincausal_data/answers/calc_kappa.py
+++ annotation/output/fincausal-results/fincausal_data/answers/calc_kappa.py
@@ -3,17 +3,19 @@
 
 import argparse
 import json
+from collections import defaultdict
 from itertools import combinations
 from typing import TextIO
 
 from sklearn.metrics import cohen_kappa_score  # type: ignore
 
 
-def main(infile: TextIO) -> None:
+def main(infile: TextIO, outfile: TextIO | None) -> None:
     data = json.load(infile)
 
     names = {a["name"] for d in data for a in d["answers"]}
     kappas: list[float] = []
+    name_to_kappas: dict[str, list[float]] = defaultdict(list)
 
     for name1, name2 in combinations(names, 2):
         answers1: list[bool] = []
@@ -31,11 +33,28 @@ def main(infile: TextIO) -> None:
 
         kappa = cohen_kappa_score(answers1, answers2)
         kappas.append(kappa)
+        name_to_kappas[name1].append(kappa)
+        name_to_kappas[name2].append(kappa)
 
         print(f"{name1:<10} {name2:<10}: {kappa:.4f} n={len(answers1)}")
 
     avg_kappa = sum(kappas) / len(kappas)
     print(f"\nAverage Cohen's Kappa: {avg_kappa:.4f}")
+
+    named_avg_kappas = sorted(
+        {
+            name: sum(kappas) / len(kappas) for name, kappas in name_to_kappas.items()
+        }.items(),
+        key=lambda x: x[1],
+        reverse=True,
+    )
+
+    for name, avg_kappa in named_avg_kappas:
+        print(f"{name:<10}: {avg_kappa:.4f}")
+
+    if outfile:
+        ordered_names = [name for name, _ in named_avg_kappas]
+        json.dump(ordered_names, outfile)
 
 
 if __name__ == "__main__":
@@ -43,5 +62,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "infile", type=argparse.FileType("r"), help="Path to the input file."
     )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=argparse.FileType("w"),
+        default=None,
+        help="Path to output file with the username ordered by highest average kappa.",
+    )
     args = parser.parse_args()
-    main(args.infile)
+    main(args.infile, args.output)
